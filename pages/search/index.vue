ea4dc7959147ae9be8d3439b283c71afd8ab2829
ZengYu <13232094481@163.com>
2021-03-26 23:32:15 +0800

    manhua

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -23,9 +23,12 @@
 					<view>热搜</view>
 				</view>
 				<view class="list_ls">
-					<view class="item"  v-for="(item,index) in hotList" :key="index" >
-						<font>{{index+1}} </font>
-					 {{item}}
+					<view class="item" style="width: 47%;"  v-for="(item,index) in hotList" :key="index" >
+						<font v-if="index<4" class="hot-index">{{index+1}} </font>
+						<font v-else class="nohot-index">{{index+1}} </font>
+						<font style="margin-right: 10rpx;">{{item.content}}</font>
+						<u-tag v-if="item.type==='hot'" text="热" mode="dark" size="mini" type="warning" />
+						<u-tag v-if="item.type==='new'" text="新" mode="dark" size="mini" bg-color="#ff6781" />
 					</view>
 				</view>
 			</view>
@@ -34,10 +37,16 @@
 					<view>搜索历史</view>
 				</view>
 				<view class="list_ls">
-					<view class="item" :class="'square round-fill'" v-for="(item,index) in hotList" :key="index" >{{item}}</view>
+					<view class="item" :class="'square round-fill'" v-for="(item,index) in hotList" :key="index" >{{item.content}}</view>
 				</view>
 			</view>
 		</view>
+		<view style="text-align: center; color: #919191; margin-top: 30rpx;" >
+			<u-icon  name="icon custom-icon custom-icon-ashbin-fill" color="#919191" size="35"></u-icon>
+			<font style="font-size: 25rpx;">清空搜索历史</font>
+		</view>
+		
+				
 	</view>
 	
 	
@@ -48,7 +57,18 @@
 		data() {
 			return {
 				keyword: '',
-				hotList:['1','2']
+				hotList:[
+					{content:'王者巴宝莉取消合作',type:''},
+					{content:'新疆棉花',type:'hot'},
+					{content:'原神莫娜',type:'hot'},
+					{content:'LGD IG',type:'new'},
+					{content:'张少华去世',type:''},
+					{content:'菜狗的原画师来了',type:''},
+					{content:'怪物猎人首发直播',type:'new'},
+					{content:'国货大揭秘',type:'new'},
+					{content:'IU新歌MV',type:''},
+					{content:'HM',type:''}
+					]
 			}
 		},
 		onLoad() {
@@ -110,13 +130,27 @@
 	.map-wrap-text {
 		padding: 0 6rpx;
 	}
-	.list{padding: 0 20rpx;
+	.list{padding: 0 15rpx;
 		.head{display: flex;  justify-content: space-between; color: #1A1A1A;font-size: 30rpx;margin-bottom: 28rpx; margin-top: 20rpx;font-weight:bold;}
-		.list_ls .item{display: inline-block;padding: 8rpx 24rpx;margin:0rpx 16rpx 22rpx 0rpx;font-size: 28rpx;color: #1A1A1A;
+		.list_ls .item{display: inline-block;padding: 8rpx 10rpx;margin:0rpx 16rpx 22rpx 0rpx;font-size: 28rpx;color: #1A1A1A;
 			&.round{border: 2rpx solid #EFEFEF;border-radius: 24rpx;font-size: 24rpx;color: #919191;}
 			&.round-fill{background-color:#EFEFEF;border: 2rpx solid #EFEFEF;border-radius: 24rpx;font-size: 24rpx;color: #919191;}
 			&.square{border: 2rpx solid #EFEFEF;border-radius: 8rpx;font-size: 24rpx;color: #919191;}
 			&.square-fill{background-color:#EFEFEF; border-radius: 8rpx;font-size: 24rpx;color: #919191;}
 		}
+	}
+	.hot-index{
+		font-weight:bold;
+		margin-right: 10rpx;
+	}
+	
+	.nohot-index{
+		margin-right: 10rpx;
+	}
+	
+	.center-flex {
+	    display: flex;
+	    flex-direction: column;
+	    justify-content: center;
 	}
 </style>
